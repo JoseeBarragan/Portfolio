b546743f403245bce11c9631fbd0e878fa720a8f
JoseeBarragan <barraganseba@gmail.com>
2026-06-01 18:27:36 -0300

    Finishing Portfolio, missing check on the responsive

--- App/src/components/Header/Header.tsx
+++ App/src/components/Header/Header.tsx
@@ -31,8 +31,6 @@ export default function Header() {
     return (
         <header className="fixed top-0 left-0 z-1000 flex items-center md:px-5 lg:px-10 py-6 w-full">
   
-            {/* Logo - izquierda */}
-            {/* Nota: cambié logoRef de HTMLParagraphElement a HTMLDivElement ya que lo aplicas a un <div> */}
             <div 
                 id="logo" 
                 ref={logoRef} 
@@ -44,7 +42,7 @@ export default function Header() {
             </div>
 
             {/* Mail - centro absoluto */}
-            <a href="#email" className="absolute left-1/2 -translate-x-1/2">
+            <a href="mailto:barraganseba@gmail.com" className="absolute left-1/2 -translate-x-1/2">
                 <div 
                     id="email" 
                     ref={emailRef} 
@@ -68,9 +66,9 @@ export default function Header() {
                         <p className="text-white group-hover:text-black font-medium">WORK</p>
                     </div>
                 </a>
-                <a href="#contact">
+                <a href="#stack">
                     <div className="group px-3 py-2 cursor-pointer" ref={contactRef} onMouseEnter={() => handleEnter(contactRef)} onMouseLeave={handleLeave}>
-                        <p className="text-white group-hover:text-black font-medium">CONTACT</p>
+                        <p className="text-white group-hover:text-black font-medium">STACK</p>
                     </div>
                 </a>
             </div>
